2e3f2b50f555d5e5cf41523ad50774eb7d26e455
TedJenkler <Tedjenkler@gmail.com>
2023-11-06 06:15:59 -0200

    Advice Logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [id, setID] = useState();
+  const [advice, setAdvice] = useState();
+
+  const handleClick = async () => {
+    try {
+        const data = await (await fetch('https://api.adviceslip.com/advice')).json()
+        setID(data.slip.id)
+        setAdvice(data.slip.advice)
+    } catch (err) {
+        console.log("API seems to be down")
+    }
+}
+
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((data) => {
+        setID(data.slip.id)
+        setAdvice(data.slip.advice)
+        console.log(data.slip)
+      }
+      );
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='container'>
+      <div className='row'>
+        <h1>Advice #{id}</h1>
+        <p>{advice}</p>
+      </div>
+      <div className='row'>
+        <button onClick={handleClick} className='btn'>dice</button>
+      </div>
+    </main>
   );
 }
 
